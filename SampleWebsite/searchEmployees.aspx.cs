159353c3430b0e8911ef80915856ece52d21c307
Marwa Gaser <marwagaser@gmail.com>
2018-08-02 18:30:36 +0300

    added view employee and services webpages

---
+++ SampleWebsite/searchEmployees.aspx.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class searchEmployees : System.Web.UI.Page
+{
+    public static int myInt = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand("viewEmployees", conn);
+
+        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+    }
+    protected void info(Object sender, GridViewCommandEventArgs e)
+    {
+        int row = Convert.ToInt32(e.CommandArgument.ToString());
+        Session["n"] = GridView1.Rows[row].Cells[3].Text;
+        string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand("viewServices", conn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.Add(new SqlParameter("@iqama", Session["n"].ToString()));//HERE
+        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        GridView2.DataSource = dt;
+        GridView2.DataBind();
+    }
+    protected void servicesCheckout(Object sender, GridViewCommandEventArgs e) {
+        int row = Convert.ToInt32(e.CommandArgument.ToString());
+        Session["n"] = GridView2.Rows[row].Cells[1].Text;
+        string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand cmd = new SqlCommand("calcSum", conn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.Add(new SqlParameter("@ServiceID", Session["n"].ToString()));//HERE
+        SqlParameter flag = cmd.Parameters.Add("@sum", SqlDbType.Int);
+        flag.Direction = ParameterDirection.Output;
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        myInt =myInt+ Convert.ToInt32(cmd.Parameters["@sum"].Value);
+        TextBox1.Text = "The sum of the prices is "+myInt;
+        conn.Close();
+    }
+
+   
+}
